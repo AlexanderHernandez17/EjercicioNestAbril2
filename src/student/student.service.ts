d0c86858e38d0d5404b6c3cd2d940ad5ca290cc4
AlexanderHernandez17 <alexander.hdez.2001@gmail.com>
2024-04-03 08:40:30 -0400

    act

---
+++ src/student/student.service.ts
@@ -0,0 +1,40 @@
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
+import { Student } from './student.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+
+
+@Injectable()
+export class StudentService {
+
+    constructor(@InjectModel(Student.name) private studentModel: Model<Student>,) {}
+
+
+    findAll(){
+        return this.studentModel.find().exec();
+    }
+
+    async create(@Body() body): Promise<Student> {
+
+        const studentData = {
+            name: body.name,
+            password: body.password
+        }
+        const student = new this.studentModel(studentData)
+        return await student.save();
+        
+    }
+
+    async findOne(id: string) {
+        const student = this.studentModel.findOne({id: id});
+        if (!Student) {
+            throw new NotFoundException(`Student #${id} not found`)
+            
+        }
+        return student;
+    }
+}
+
+
+
